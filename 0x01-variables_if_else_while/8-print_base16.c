d827690cc4a5be304c5c18ae2e5cb1f36973564d
Louis batty <battylouis64@gmail.com>
2022-06-03 09:49:32 -0700

    Task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = '0'; letter <= '9'; letter++)
+		putchar(letter);
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
