79f77dbe6f6fccb8b4371d128b1dcad9a5c1fa1e
Louis batty <battylouis64@gmail.com>
2022-06-03 09:28:35 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)//usese asscii chart
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
