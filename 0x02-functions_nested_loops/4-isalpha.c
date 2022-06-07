37bf55ef8ccde61deb2d308b014942c24b8cbc9c
Louis batty <battylouis64@gmail.com>
2022-06-07 03:27:16 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - checkes if the charecter is alphabetic
+ * @c: Singel letter 
+ * Return: 1 if its a letter , 0 if not
+ */
+int _isalpha(int c)
+{
+	if (((c <= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+		return (1);
+	else
+		return (0);
+}
