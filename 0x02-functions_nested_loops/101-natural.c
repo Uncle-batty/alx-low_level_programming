e9a40481cdea8ccb1289fa7e9858425033766168
Louis batty <battylouis64@gmail.com>
2022-06-07 05:39:42 -0700

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry poit
+ *
+ * Return: alwasy 0
+ */
+int main(void)
+{
+	int k, sum;
+	for (k = 0; k < 1024; k++)
+	{
+		if ((k % 3 == 0) || (k % 5 == 0))
+		{
+			sum += k;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	printf(sum);
+}
