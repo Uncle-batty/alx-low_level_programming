62cbe51cf3cc5a1792e0f931489adc5a2f2742d2
Louis batty <battylouis64@gmail.com>
2022-06-07 04:02:32 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - is a function to return the absolute value of an int
+ * @num: a int
+ * Return: absolute value
+ */
+int _abs(int num)
+{
+	return (num * (( num > 0) - (numb <0)));
+}
