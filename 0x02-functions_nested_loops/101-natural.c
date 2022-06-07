e83c3f9c0371f708f6c0fc4b8bad9c131a10797e
Louis batty <battylouis64@gmail.com>
2022-06-07 05:48:06 -0700

    fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	int k, sum;
+
 	for (k = 0; k < 1024; k++)
 	{
 		if ((k % 3 == 0) || (k % 5 == 0))
@@ -19,6 +20,6 @@ int main(void)
 			continue;
 		}
 	}
-	printf("%d",sum);
+	printf("%d\n", sum);
 	return (0);
 }
