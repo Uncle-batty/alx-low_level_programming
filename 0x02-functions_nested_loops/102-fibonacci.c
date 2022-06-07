034025bb397bfc8c8a782e6ffd2173ced7daf361
Louis batty <battylouis64@gmail.com>
2022-06-07 05:58:48 -0700

    TAsk 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int k, num1, num2, fib;
+
+	num1 = 1;
+	num2 = 1;
+	printf("%d, %d, ", num1, num2);
+	for (k = 0; k < 48; k++)
+	{
+		fib = num1 + num2;
+		num1 = num2;
+		num2 = fib;
+		print("%d", fib);
+		if (k < 48)
+			printf(", "
+	}
+	printf("\n");
+	return (0);
+}
