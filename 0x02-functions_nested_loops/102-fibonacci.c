50a3649d41406dd874401b0a80baaa908ff8d84d
Louis batty <battylouis64@gmail.com>
2022-06-07 06:07:52 -0700

    NEw 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,13 +10,13 @@ int main(void)
 
 	num1 = 1;
 	num2 = 1;
-	printf("%d, ", num2);
+	printf("%ld, ", num2);
 	for (k = 0; k < 48; k++)
 	{
 		fib = num1 + num2;
 		num1 = num2;
 		num2 = fib;
-		printf("%d", fib);
+		printf("%ld", fib);
 		if (k < 48)
 			printf(", ");
 	}
