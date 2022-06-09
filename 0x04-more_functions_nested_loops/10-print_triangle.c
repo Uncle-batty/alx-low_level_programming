d5480026622a6703d648a0ad5a83e4f3de6245ff
Louis batty <battylouis64@gmail.com>
2022-06-09 04:59:43 -0700

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - makes a triagle
+ * @size: is the varisble for size
+ * Return: non
+ */
+void print_triangle(int size)
+{
+	int x, y;
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y <= size; y++)
+			{
+				if (y <= size -x)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
