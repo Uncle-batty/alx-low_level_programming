78aef208f9b2e1fb8fd31adb37505dcd71cd7f0b
Louis batty <battylouis64@gmail.com>
2022-06-09 03:37:24 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ *
+ * Returns: no return
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
