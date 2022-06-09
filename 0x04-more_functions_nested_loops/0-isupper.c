e77d136b01c89ba63e3eb5569194f7d80a96fb19
Louis batty <battylouis64@gmail.com>
2022-06-09 02:30:04 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase 
+ * @c: char variable
+ * Return: 1 if upper 0 if not
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}c
