b860ac55e5b19924bac561ee0266395045b5fd47
Louis batty <battylouis64@gmail.com>
2022-06-09 04:06:40 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Prints a line of n size _
+ * @n: var for lentgh
+ * Return: no return
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		for (x; a <= n; x++)
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
+}
