8423fbb7d335b0fcd10cc59e8e7e9fa11da1d4df
Louis batty <battylouis64@gmail.com>
2022-06-13 03:04:44 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - a fuction to reverse a string
+ * @s : string input
+ * Return: string in revers
+ */
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int counter = 0, i, k;
+	
+	for (i = 0; s[counter] != 0; i++)
+	{
+		counter++;
+	}
+
+	for (k = 0; k < counter; k++)
+	{
+		counter--;
+		rev = s[k];
+		s[k] = s[counter];
+		s[counter] = rev;
+	}
+}
