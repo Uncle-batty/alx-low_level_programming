a6ef7dbc0396da89b499565846d200ff224a7eaf
Louis batty <battylouis64@gmail.com>
2022-06-13 02:12:56 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - a function to chage the pointer to 98
+ * @n: input pointer
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
