bd1a4ae6fcb13fb47b6d85825cdbf0aeb522ebc8
Louis batty <battylouis64@gmail.com>
2022-06-13 02:29:24 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - A function to swop two int values
+ * @a: input 1
+ * @b: input 2
+ * Return: swoped int
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = n;
+}
